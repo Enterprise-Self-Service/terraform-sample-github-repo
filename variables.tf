2407741fb8457b191621b7cb5a621ba9fc968a61
Richard Jones <richard.jones@cloudtechnologyoffice.com>
2024-01-26 12:19:50 +0000

    first commit

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "name" {
+    type = string
+    description = "Repository name"
+}
+
+variable "description" {
+    type = string
+    description = "Repository description"
+}
+
+variable "visibility" {
+    type = string
+    description = "Either public or private"
+}
+
